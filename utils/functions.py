90098fdb0b35dfdad941ca979f4738b5815dd502
Alex1iv <ivanov.politehnik@gmail.com>
2023-03-27 00:00:37 +0600

    initial commit

---
+++ utils/functions.py
@@ -0,0 +1,15 @@
+# Custom functions
+import pandas as pd
+import numpy as np
+
+
+def get_descr(feature:str, feature_names:pd.DataFrame=features_dscr)->str:
+    """Get feature description easily
+
+    Args:
+        feature (str): feature name
+
+    Returns:
+        str: description 
+    """
+    print(feature_names[feature_names['LoanStatNew']==feature]['Description'][0])
